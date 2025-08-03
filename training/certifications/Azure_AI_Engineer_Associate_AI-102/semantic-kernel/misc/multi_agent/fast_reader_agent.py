07d0b093f3107de7360f4820b2d5f9345ba29d8e
wxz <weixianzhang@outlook.com>
2025-08-03 23:03:53 +0800

    multi-agent group chat with KernelFunctionSelectionStrategy and KernelFunctionFromPrompt

--- training/certifications/Azure_AI_Engineer_Associate_AI-102/semantic-kernel/misc/multi_agent/fast_reader_agent.py
+++ training/certifications/Azure_AI_Engineer_Associate_AI-102/semantic-kernel/misc/multi_agent/fast_reader_agent.py
@@ -1,133 +1,239 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 from azure.identity.aio import DefaultAzureCredential
-from semantic_kernel.agents import AzureAIAgent, AgentGroupChat
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion, OpenAIChatPromptExecutionSettings
+from semantic_kernel.agents import ChatCompletionAgent, AgentGroupChat
 from semantic_kernel import Kernel
-from semantic_kernel.agents.strategies import TerminationStrategy, SelectionStrategy, KernelFunctionSelectionStrategy
+from semantic_kernel.contents import ChatHistoryTruncationReducer, ChatMessageContent, AuthorRole
+from semantic_kernel.agents.strategies import (TerminationStrategy, KernelFunctionSelectionStrategy, SequentialSelectionStrategy, SelectionStrategy,
+                                               KernelFunctionTerminationStrategy)
 from semantic_kernel.functions import kernel_function, KernelFunctionFromPrompt
 from semantic_kernel.contents import ChatHistoryTruncationReducer
 import asyncio
 
 load_dotenv(find_dotenv())
 
-project_client = AzureAIAgent.create_client(
-        #https://ai102-ai-foundry-aiservices.services.ai.azure.com/api/projects/ai102-ai-foundry-aiservices
-        endpoint='https://ai102-ai-foundry-aiservices.services.ai.azure.com/api/projects/ai102-ai-foundry-aiservices', #os.getenv("PROJECT_ENDPOINT_STRING"),
-        credential=DefaultAzureCredential()
-    )
+# project_client = AzureAIAgent.create_client(
+#         #https://ai102-ai-foundry-aiservices.services.ai.azure.com/api/projects/ai102-ai-foundry-aiservices
+#         endpoint='https://ai102-ai-foundry-aiservices.services.ai.azure.com/api/projects/ai102-ai-foundry-aiservices', #os.getenv("PROJECT_ENDPOINT_STRING"),
+#         credential=DefaultAzureCredential()
+#     )
 
 
 # code ref
 # https://github.com/MicrosoftLearning/mslearn-ai-agents/blob/main/Labfiles/05-agent-orchestration/Python/agent_chat.py
-async def create_agent(name, description, instruction) -> AzureAIAgent:
+# https://learn.microsoft.com/en-us/semantic-kernel/support/archive/agent-chat?pivots=programming-language-python
+# https://systenics.ai/blog/2025-04-22-understanding-selection-and-termination-strategy-functions-in-dotnet-semantic-kernel-agent-framework/
 
-    agent = AzureAIAgent(
-        name=name,
-        client=project_client,
-        definition= await project_client.agents.create_agent(
-            model='gpt-4',
-            name =name,
-            description=description,
-            instructions= instruction
-        )
-    )
+# was using AI Agent but switch to ChatCompletionAgent
+# async def create_agent(name, description, instruction) -> ChatCompletionAgent:
 
-    return agent
+#     agent = ChatCompletionAgent(
+#         name=name,
+#         client=project_client,
+#         definition= await project_client.agents.create_agent(
+#             model='gpt-4',
+#             name =name,
+#             description=description,
+#             instructions= instruction
+#         )
+#     )
+
+#     return agent
+
+
+# custom selection strategy
+class AgentSelectionStrategy(SelectionStrategy):
+    """Custom selection strategy that selects the next agent based on the last agent's response."""
+
+    async def select_agent(self, agents, history):
+        """Select the next agent to interact with."""
+        if not history:
+            return agents[0]  # If no history, return the first agent
+
+        last_message = history[-1]
+        if "__ALL_PAPER_END__" in last_message.content:
+            return agents[-1]  # If last message indicates all papers are done, return the summarization agent
+
+        return agents[0]  # Otherwise, return the first agent
     
 
-class SummarizedTerminationStrategy(TerminationStrategy):
-    """Custom termination strategy that terminates when the last agent has responded."""
+# class SummarizedTerminationStrategy(TerminationStrategy):
+#     """Custom termination strategy that terminates when the last agent has responded."""
 
-    async def should_agent_terminate(self, agent, history):
-        """Check if the agent should terminate."""
-        return "__ALL_SUMMARY_END__" in history[-1].content
+#     async def should_agent_terminate(self, agent, history):
+#         """Check if the agent should terminate."""
+#         return "__ALL_SUMMARY_END__" in history[-1].content
+    
+
 
 
 
 async def main():
 
     kernel = Kernel()
+   
 
-    fast_reader_agent_desc = '''A fast reader agent that makes use of other registered agents to get story books first page,
-    and summarizes book content in a few sentences.
-    This agent will return the summary as a termination agent'''
-    fast_reader_agent = await create_agent(
-        name="fast_reader_agent",
-        description=fast_reader_agent_desc,
-        instruction=fast_reader_agent_desc
+    chat_completion_service = AzureChatCompletion(
+        service_id="default",
+        deployment_name="gpt-4",
+        api_version='2025-01-01-preview',
+        api_key=os.getenv("openai_key"),
+        endpoint=os.getenv("PROJECT_ENDPOINT_STRING")
+    )
+
+    kernel.add_service(service=chat_completion_service)
+
+
+    genai_paper_agent_name = "genai_paper_agent"
+    summarize_paper_agent_name = "summarize_paper_agent"
+
+
+    # fast_reader_agent_desc = '''A fast reader agent that makes use of other registered agents to get story books first page,
+    # and summarizes book content in a few sentences.
+    # This agent will return the summary as a termination agent'''
+    # fast_reader_agent = ChatCompletionAgent(
+    #     name='fast_reader_agent',
+    #     kernel=kernel,
+    #     description= fast_reader_agent_desc,
+    #     instructions= fast_reader_agent_desc,
+    # )
+
+
+    genai_paper_agent_desc = """A generative ai paper agent that retrieves generative ai papers based on user requests. 
+    "Retrieves top 3 short generative ai papers that you think are most releveant to user's request."""
+    genai_paper_agent = ChatCompletionAgent(
+        name=genai_paper_agent_name,
+        kernel=kernel,
+        instructions= genai_paper_agent_desc,
+    )
+
+    summarize_paper_agent_desc = """A summarization agent that provides concise summary of all 3 papers.
+    Each paper is summarized to 5 lines or less.
+    The summary is returned as a single response ending with the word "__ALL_SUMMARY_END__"."""
+    summarize_paper_agent = ChatCompletionAgent(
+        name=summarize_paper_agent_name,
+        kernel=kernel,
+        instructions= summarize_paper_agent_desc,
     )
 
 
-    story_book_agent_desc = """A story book agent that retrieves story books based on user requests. 
-    "Retrieves top 3 story book first page that you think are most releveant to user's request.
-    For each story book first page, each separated with __STORY_BOOK_END_. 
-    the final content result of al 3 story book first page, '__ALL_STORY_BOOK_END__' to mark the end of the content."""
-    story_book_agent = await create_agent(
-        name="story_book_agent",
-        description=story_book_agent_desc,
-        instruction=story_book_agent_desc,
-        
-    )
-
-    summarize_book_agent_desc = """A summarization agent that provides concise summaries of all 3 story books first paragraphs.
-    Each story book first page is separated by __STORY_BOOK_END_, summarize each story book first page in 5 lines or less.
-    Each summarized story book first page is separated by __SUMMARY_END_,
-    The final content result of all 3 story book first page summaries, '__ALL_SUMMARY_END__' to mark the end of the content."""
-    summarize_book_agent = await create_agent(
-        name="summarize_book_agent",
-        description=summarize_book_agent_desc,
-        instruction=summarize_book_agent_desc,
-    )
-
+    # and begin to summarize each of the first page of top 3 story books returned by {genai_paper_agent_name} agent, and return the summarized results.
+    #                - {genai_paper_agent_name} is the one that can retrieve top 3 story book first page and return the results ending with __ALL_PAPER_END__.
     selection_function = KernelFunctionFromPrompt(
                 function_name="selection",
                 prompt=f"""
-                You are an agent selection function, examine theprovided response, if you see __ALL_STORY_BOOK_END__ in response,
-                means story-book agent has completed retrieving story book and next summarizer agent should take over.
-                If you do not see __ALL_STORY_BOOK_END__ means no story book is retrieved yet, so the story book agent should continue to take over.
+                Determine which participant takes the next turn in a conversation based on the the most recent participant.
+                State only the name of the participant to take the next turn, with no breakline or space.
+                No participant should take more than one turn in a row.
                 
                 Choose only from these participants:
-                - {story_book_agent}
-                - {summarize_book_agent}
+                - {genai_paper_agent_name}
+                - {summarize_paper_agent_name}
 
-                Rules:
-                - {story_book_agent} is the one that can retrieve top 3 story book first page and return the results whenever user asks about story books.
-                - {summarize_book_agent} is the one that can summarize the first page of top 3 story books and return the results whenever user asks for summaries.
+                - {genai_paper_agent_name} will start.
+                - If response contains all paper content, select {summarize_paper_agent_name} agent for next response.
+
 
                 History:
-                {{$history}}
+                {{$lastmessage}}
                 """
             )
     
     termination_function = KernelFunctionFromPrompt(
                 function_name="termination",
                 prompt=f"""
-                if you see __ALL_SUMMARY_END__ in response, means summarization agent has completed summarizing story book first page and the conversation can be terminated.
-                If you do not see __ALL_SUMMARY_END__ means summarization agent has not completed
+                
+                If response contains the word "__ALL_SUMMARY_END__", reply with only one word "done"
+
+                History
+                {{$lastmessage}}
                 """)
 
-    kernel = Kernel()
+    selection_result_parser = lambda result: (
+        # print(f"Selection result: {result.value}")
+        # [print(f'role:{v["role"]}, content:{v["content"]}') for v in result.metadata['arguments']['lastmessage']]
+        # print('\n\n')
+        result.value[0].content.strip() if result and result.value else summarize_paper_agent_name
+    )
+
 
     agc = AgentGroupChat(
-        agents=[fast_reader_agent, story_book_agent, summarize_book_agent],
-        termination_strategy=SummarizedTerminationStrategy(),
+        agents=[genai_paper_agent, summarize_paper_agent],
+        
+        termination_strategy= KernelFunctionTerminationStrategy(
+            agents= [summarize_paper_agent],
+            agent_variable_name="agents",
+            kernel=kernel,
+            function=termination_function,
+            result_parser=lambda result: (
+                True if result.value[0].content == "done" else False
+            ),
+            history_variable_name="lastmessage",
+            #history_reducer=ChatHistoryTruncationReducer(target_count=1),
+            #maximum_iterations=2
+        ),
+
+        # selection_strategy=AgentSelectionStrategy()
+        
         selection_strategy=KernelFunctionSelectionStrategy(
-            initial_agent=fast_reader_agent,
+            initial_agent=genai_paper_agent,
             agent_variable_name="agents",
             kernel=kernel,
             function=selection_function,
-            result_parser=lambda result: result.content,
-             history_variable_name="history",
-             history_reducer=ChatHistoryTruncationReducer(target_count=5)
-        ))
+            result_parser=lambda result: (
+            # print(f"Selection result: {result.value}")
+            # [print(f'role:{v["role"]}, content:{v["content"]}') for v in result.metadata['arguments']['lastmessage']]
+            # print('\n\n')
+            result.value[0].content.strip() if result and result.value else summarize_paper_agent_name
+        ),
+            history_variable_name="lastmessage",
+            #history_reducer=ChatHistoryTruncationReducer(target_count=1)
+        )
 
-    response = await agc.add_chat_message("I would like to summarize top 3 Stephen King horror story book content")
+        # selection_strategy=SequentialSelectionStrategy(
+        #     agents=[genai_paper_agent, summarize_paper_agent],
+        #     initial_agent=genai_paper_agent
+        # )
+    )
 
-    print(response)
 
-    await project_client.agents.delete_agent(fast_reader_agent.name)
-    await project_client.agents.delete_agent(story_book_agent.name)
-    await project_client.agents.delete_agent(summarize_book_agent.name)
+   
+
+    # chat completion test
+    # chat_history = ChatHistory(
+    #     messages=[ChatMessageContent(AuthorRole.USER, content="I would like to summarize top 3 Stephen King horror story book content")]
+    # )
+    # response = await chat_completion_service.get_chat_message_content(
+    #     chat_history=chat_history,
+    #     settings= OpenAIChatPromptExecutionSettings(top_p=0.1, temperature=0.7, max_tokens=1000)
+    #     )
+
+    # print(response)
+    await agc.add_chat_message(ChatMessageContent(AuthorRole.USER, content="I would like to retrieve and summarize any 3 generative AI paper that is used in real world to solve problems like in healthcare or finance or cybersecurity."))
+    
+
+    try:
+        # Invoke a response from the agents
+        async for response in agc.invoke():
+            if response is None or not response.name:
+                continue
+            # print(f"{response.content}")
+    except Exception as e:
+        print(f"Error during chat invoke: {e}")
+
+    # print summary only
+    async def get_first_element(async_iterable):
+        async for item in async_iterable:
+            return item
+        
+
+    first_item = await get_first_element(agc.get_chat_messages())
+    print(f"Chat History:{first_item.content}")
+
+    # await project_client.agents.delete_agent(fast_reader_agent.name)
+    # await project_client.agents.delete_agent(genai_paper_agent.name)
+    # await project_client.agents.delete_agent(summarize_paper_agent.name)
 
 
 asyncio.run(main())
