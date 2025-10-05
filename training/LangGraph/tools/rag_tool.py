014691697cb6d2f4c3ea70ac3f18db3dd101b70d
Weixian Zhang <weixianzhang@outlook.com>
2025-10-05 22:54:22 +0800

    added Supervisor agent pattern example

---
+++ training/LangGraph/tools/rag_tool.py
@@ -0,0 +1,48 @@
+import os
+from langchain.document_loaders import PyPDFLoader
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import AzureOpenAIEmbeddings
+from langchain.tools import tool
+from dotenv import load_dotenv
+
+load_dotenv()
+# https://blog.futuresmart.ai/langgraph-agent-with-rag-and-nl2sql#heading-agentic-rag-tool-integration
+
+def load_document():
+    doc_path = os.path.join(os.path.dirname(__file__), "./azure_fundamentals.pdf")
+    loader = PyPDFLoader(doc_path)
+    document = loader.load()
+    return document
+
+
+documents = load_document()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+    length_function=len
+)
+
+chunks = text_splitter.split_documents(documents)
+
+collection_name = "azure_fundamentals"
+
+
+embeddings = AzureOpenAIEmbeddings()
+
+
+vectorstore = Chroma.from_documents(documents=chunks, 
+                                    collection_name=collection_name,
+                                    embedding=embeddings
+                                    )
+
+
+def rag_tool(query: str) -> str:
+    docs = vectorstore.similarity_search(query, k=5)
+    context = "\n".join([doc.page_content for doc in docs])
+    return context
+
+
+
+rag_tool('this book is published by who or which company?')
